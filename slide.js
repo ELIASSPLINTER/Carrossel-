f3e961198c3c562bb4786740c945910637b56031
ELIASSPLINTER <eliascribeiro.dev@gmail.com>
2022-12-26 18:01:36 -0300

    adicionado novos eventos

--- slide.js
+++ slide.js
@@ -2,50 +2,61 @@ export default class Slide{
     constructor(slide,wrapper){
         this.slide = document.querySelector(slide)
         this.wrapper = document.querySelector(wrapper);
+        this.dist = {finalPosition:0, startX:0, movement:0}
     }
-}
-
-onStart(event){
-    event.preventDefault();
-    console.log(this);
-}
-
-onmouseover(event) {
     
-}
+    onStart(event){
+        event.preventDefault();
+        this.dist.startX= event.clientX;
+        
+        this.wrapper.addEventListener('mousemove', this.onMove);
+    }
 
-
-addSlideEvents(){
-    this.wrapper.addEventListener('mousedown',this onStart);
-
-}
+    onMove(event){
+        console.log(this.dist.startX - event.clientX)
+    }
+    
+    onEnd(event){  
+        console.log("entrou");
+        this.wrapper.removeEventListener('mausemove', this.onMove);
+        
+    }
+    
+    addSlideEvents(){
+        this.wrapper.addEventListener('mousedown', this.onStart);
+        this.wrapper.addEventListener('mouseup', this.onEnd);
+        
+    }
  
-bindEvents() {
-    this.onStart = this.onStart.bind(this);
+    bindEvents() {
+        this.onStart = this.onStart.bind(this);
+        this.onMove = this.onMove.bind(this);
+        this.onEnd = this.onEnd.bind(this);
+    }
+
+    init(){
+        this.bindEvents();
+        this.addSlideEvents();
+        this.slideConfig();
+        return this;
+    }
+
+
+
 }
-
-init(){
-    this.bindEvents();
-    this.addSlideEvents();
-    this.slideConfig();
-    return this;
-}
-
-
-
 
 
 
 
 
 // lideConfig(){
-//     this.slideArray = [...this.slideArray.childzem].map((Element) => {
-//         const position = element.offsetLeft;
-//         return{
-//             position,
-//             element
-//         }
-//     });
+    //     this.slideArray = [...this.slideArray.childzem].map((Element) => {
+        //         const position = element.offsetLeft;
+        //         return{
+            //             position,
+            //             element
+            //         }
+            //     });
 // }
 
 // slidePosition(slide) {
